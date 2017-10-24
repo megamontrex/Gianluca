ca51d526f097d99e9cd52a43d90d12477b019fd8
Gianluca Anelli <a101097@mcast.org.mt>
2017-10-24 14:45:37 +0200

    BlockBuster and number wizard ui

---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void LoadLvl(string LvlName)
+    {
+        print("Loading lvl" + LvlName);
+
+        SceneManager.LoadScene(LvlName);
+    }
+    public void Exit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class NumberWizard : MonoBehaviour
+{
+
+    //creates 3 number variables
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
+    int MaxNmberofTries = 10;
+
+    public Text guessText;
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (Random.Range(min,max));
+
+
+        guessText.text = guess.ToString();
+
+        MaxNmberofTries--;
+        if (MaxNmberofTries == 0)//if Ai doesnt guess the number you win
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+}
+        	
+	
+	

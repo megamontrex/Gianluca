ba483a4fec53ffbe0cf7e27fb015f1073f7c70b1
Gianluca Anelli <a101097@mcast.org.mt>
2017-10-26 09:50:17 +0200

    Block buster Update

---
+++ BlockBreaker/Assets/Scripts/MusicPlaer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlaer : MonoBehaviour {
+
+    static MusicPlaer myMusicPlayer = null;
+
+    void Awake()
+    {
+        if (myMusicPlayer == null)
+        {
+
+
+
+            myMusicPlayer = this;
+            print("Sound object" + this.gameObject.GetInstanceID().ToString());
+            print("First Object");
+            //this 
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            print("Sound object" + this.gameObject.GetInstanceID().ToString());
+
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start() {
+        
+
+		
+	}
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
